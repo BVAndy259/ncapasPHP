45763f12ff203f604aec7aef72e2d52349eebb44
ElKratos259 <elkratosxdd@outlook.com>
2025-07-08 15:51:16 -0500

    datos/DB.php

--- datos/DB.php
+++ datos/DB.php
@@ -1,8 +1,8 @@
 <?php
     class DB{
-        private $url='mysql:host=127.0.0.1;dbname=ventasweb1';
-        private $user='root';
-        private $password='';
+        private $url='cadena de conexion a la base de datos';
+        private $user='usuario';
+        private $password='contraseña';
         public function conectar(){
             $cn=new PDO($this->url,$this->user,$this->password);
             return $cn;
